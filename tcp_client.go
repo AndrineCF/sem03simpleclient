808bc938e22d8bedd842ab232d870d5351cfc0c3
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-04-21 10:53:38 +0000

    endret til poxy

--- tcp_client.go
+++ tcp_client.go
@@ -8,13 +8,14 @@ import (
 )
 
 func main() {
-        conn, err := net.Dial("tcp", "172.17.0.2:4000")
+        conn, err := net.Dial("tcp", "172.17.0.4:4000")
         if err != nil {
                 log.Fatal(err)
         }
 
         log.Println("os.Args[1] = ", os.Args[1])
 
+	log.Println(os.Args[1])
 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 	log.Println("Kryptert melding: ", string(kryptertMelding))
 	_, err = conn.Write([]byte(string(kryptertMelding)))
@@ -26,6 +27,9 @@ func main() {
         if err != nil {
                 log.Fatal(err)
         }
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
         response := string(buf[:n])
         log.Printf("reply from proxy: %s", response)
 }
