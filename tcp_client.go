a6d1c6146e4984f440e234794b5e50aa958acc01
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-04-21 07:10:31 +0000

    laget endringer til oppgave 2

--- tcp_client.go
+++ tcp_client.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-	"net"
-	"log"
-	"os"
+        "net"
+        "log"
+        "os"
 	"github.com/AndrineCF/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.3:4000")
-	if err != nil {
-		log.Fatal(err)
-	}
-    	
-	// vis kva som blir sendt inn
-	log.Println("os.Args[1] = ", os.Args[1])
+        conn, err := net.Dial("tcp", "172.17.0.2:4000")
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        log.Println("os.Args[1] = ", os.Args[1])
 
 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 	log.Println("Kryptert melding: ", string(kryptertMelding))
-	
-	// skriver til server
 	_, err = conn.Write([]byte(string(kryptertMelding)))
-	if err != nil {
-		log.Fatal(err)
-	}
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+        if err != nil {
+                log.Fatal(err)
+        }
+        buf := make([]byte, 1024)
+        n, err := conn.Read(buf)
+        if err != nil {
+                log.Fatal(err)
+        }
+        response := string(buf[:n])
+        log.Printf("reply from proxy: %s", response)
 }
